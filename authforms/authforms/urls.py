b39ab40a0a899704a5614de7d5bc30872afd0b07
robert-jan <lub@countcooper.nl>
2019-04-08 13:57:43 +0800

    making connection to AWS database

--- authforms/authforms/urls.py
+++ authforms/authforms/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+app_name = "authforms"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("authforms_app.urls")),
